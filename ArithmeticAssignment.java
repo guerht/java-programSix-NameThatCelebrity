9f5e6811adaac4349d7f4a3079bb1b6c51e9bbde
guerht <parkseunghoon@gpa.ac.kr>
2015-09-07 14:00:16 +0900

    MATHS ADDED

---
+++ ArithmeticAssignment.java
@@ -0,0 +1,13 @@
+/*
+ * Project: ArithmeticAssignment.java
+ * Description: Cheating on maths homework
+ * Author: Seunghoon Park
+ * Date: 7 September, 2015
+ */
+public class ArithmeticAssignment {
+	public static void main(String args[]) {
+		System.out.println("79 + 3 * (4 + 82 - 68) -7 + 19 = "+(79+3*(4+82-68)-7+19));
+		System.out.println("(179 + 21 + 10) / 7 + 181 = "+((179+21+10)/7+181));
+		System.out.println();
+	}
+}
